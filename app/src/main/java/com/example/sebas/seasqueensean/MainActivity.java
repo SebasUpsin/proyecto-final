f86d9cdf7bd7b29146fc6f782b7271fc68f0b954
SebasUpsin <2016030148@upsin.edu.mx>
2019-03-11 18:00:24 -0700

    Creacion y Diseño de la pagina principal, asi como agregacion de archivos drawable para el diseño de botones, y se agregaron las imagenes correspondientes para los iconos

--- app/src/main/java/com/example/sebas/seasqueensean/MainActivity.java
+++ app/src/main/java/com/example/sebas/seasqueensean/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sebas.seasqueensean;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this,MenuPrincipalActivity.class);
+        startActivity(intent);
     }
 }
