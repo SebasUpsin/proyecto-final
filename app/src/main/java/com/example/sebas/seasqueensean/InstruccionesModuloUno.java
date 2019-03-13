f5e4da054359688a07bf490aba04ad77018ad9fc
luisdavidroman <2016030130@upsin.edu.mx>
2019-03-13 09:20:39 -0700

    Se agrego layout de instrucciones del modulo 1

---
+++ app/src/main/java/com/example/sebas/seasqueensean/InstruccionesModuloUno.java
@@ -0,0 +1,13 @@
+package com.example.sebas.seasqueensean;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstruccionesModuloUno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instrucciones_modulo_uno);
+    }
+}
