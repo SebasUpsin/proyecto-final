550b0ef58eb49e770237c69273ab44773d7f4055
Josa9osi <josa9osi@gmail.com>
2019-03-30 01:34:50 -0700

    Actividad 2

--- app/src/main/java/com/example/sebas/seasqueensean/ActividadDos.java
+++ app/src/main/java/com/example/sebas/seasqueensean/ActividadDos.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActividadDos extends AppCompatActivity {
 
@@ -23,12 +24,24 @@ public class ActividadDos extends AppCompatActivity {
     private Button btn6;
     private Button btn7;
     private Button btn8;
+    private Button btn9;
+    private Button btn10;
+    private Button btn11;
+    private Button btn12;
     private Button btnBorrar;
     private ProgressBar barra;
     private Thread hilo=null;
     private String nombre;
     private String edad;
+    private String[][] palabras={{"HOLA","CASA","RAMO","CAMA","CARRO"},
+            {"EURO","HILO","AMOR","LLAMA","CLASE"},
+            {"SOCIAL","CHILE","PELO","PRIMO","PLUMA"},
+            {"PRESA","CLÁUSULA","MUSICA","RARAMURI","HEROES"},
+            {"ESCUELA","EMPRESA","MOCHILA","PRIMARIA","PROCLAMAR"}};
+    private int palabra;
+    private int contador;
     private int x;
+    private int j;
     private boolean running;
 
     @Override
@@ -51,8 +64,14 @@ public class ActividadDos extends AppCompatActivity {
         this.btn6 = (Button) findViewById(R.id.btn6);
         this.btn7 = (Button) findViewById(R.id.btn7);
         this.btn8 = (Button) findViewById(R.id.btn8);
+        this.btn9 = (Button) findViewById(R.id.btn9);
+        this.btn10 = (Button) findViewById(R.id.btn10);
+        this.btn11 = (Button) findViewById(R.id.btn11);
+        this.btn12 = (Button) findViewById(R.id.btn12);
         this.btnBorrar = (Button) findViewById(R.id.btnBorrar);
         this.barra = (ProgressBar) findViewById(R.id.barra);
+        this.contador=0;
+        this.seleccionarPalabra();
         this.barra.setProgress(0);
         this.barra.setMax(6000);
         this.hilo = new Thread() {
@@ -80,60 +99,244 @@ public class ActividadDos extends AppCompatActivity {
         this.btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+            if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
                 txtTexto.setText(txtTexto.getText().toString()+"A");
+            if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                    contador++;
+                    txtTexto.setText("");
+                    if (contador == 5){
+                        x = 6000;
+                        return;
+                    }
+                    j=0;
+                    seleccionarPalabra();
+                }
             }
         });
         this.btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+            if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
                 txtTexto.setText(txtTexto.getText().toString()+"L");
+            if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                    contador++;
+                    txtTexto.setText("");
+                    if(contador==5){
+                        x=6000;
+                        return;
+                    }
+                    j=0;
+                    seleccionarPalabra();
+                }
             }
         });
         this.btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+            if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
                 txtTexto.setText(txtTexto.getText().toString()+"H");
+            if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                    contador++;
+                    txtTexto.setText("");
+                    if(contador==5){
+                        x=6000;
+                        return;
+                    }
+                    j=0;
+                    seleccionarPalabra();
+                }
             }
         });
         this.btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtTexto.setText(txtTexto.getText().toString()+"L");
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+            if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
+                txtTexto.setText(txtTexto.getText().toString()+"R");
+            if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                if(txtTexto.getText().toString().equals(palabras[contador])) {
+                    contador++;
+                    txtTexto.setText("");
+                    if(contador==5){
+                        x=6000;
+                        return;
+                    }
+                    j=0;
+                    seleccionarPalabra();
+                }
             }
         });
         this.btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtTexto.setText(txtTexto.getText().toString()+"S");
+            if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
+               txtTexto.setText(txtTexto.getText().toString()+"S");
+            if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                    contador++;
+                    txtTexto.setText("");
+                    if(contador==5){
+                        x=6000;
+                        return;
+                    }
+                    j=0;
+                    seleccionarPalabra();
+                }
             }
         });
         this.btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+            if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
                 txtTexto.setText(txtTexto.getText().toString()+"O");
+            if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                    contador++;
+                    txtTexto.setText("");
+                    if(contador==5){
+                        x=6000;
+                        return;
+                    }
+                    j=0;
+                    seleccionarPalabra();
+                }
             }
         });
         this.btn7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+            if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
                 txtTexto.setText(txtTexto.getText().toString()+"C");
+            if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                    contador++;
+                    txtTexto.setText("");
+                    if(contador==5){
+                        x=6000;
+                        return;
+                    }
+                    j=0;
+                    seleccionarPalabra();
+                }
             }
         });
         this.btn8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+            if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
                 txtTexto.setText(txtTexto.getText().toString()+"I");
+            if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                    contador++;
+                    txtTexto.setText("");
+                    if(contador==5){
+                        x=6000;
+                        return;
+                    }
+                    j=0;
+                    seleccionarPalabra();
+                }
             }
         });
+        this.btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+            if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
+                txtTexto.setText(txtTexto.getText().toString()+"E");
+            if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                    contador++;
+                    txtTexto.setText("");
+                    if(contador==5){
+                        x=6000;
+                        return;
+                    }
+                    j=0;
+                    seleccionarPalabra();
+                }
+            }
+        });
+        this.btn10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+            if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
+                txtTexto.setText(txtTexto.getText().toString()+"M");
+            if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                    contador++;
+                    txtTexto.setText("");
+                    if(contador==5){
+                        x=6000;
+                        return;
+                    }
+                    j=0;
+                    seleccionarPalabra();
+                }
+            }
+        });
+        this.btn11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+                if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
+                    txtTexto.setText(txtTexto.getText().toString()+"P");
+                if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                    if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                        contador++;
+                        txtTexto.setText("");
+                        if(contador==5){
+                            x=6000;
+                            return;
+                        }
+                        seleccionarPalabra();
+                    }
+            }
+        });
+
+        this.btn12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActividadDos.this, "simon", Toast.LENGTH_SHORT).show();
+                if(palabras[contador][palabra].length()>txtTexto.getText().toString().length())
+                    txtTexto.setText(txtTexto.getText().toString()+"U");
+                if(palabras[contador][palabra].length()==txtTexto.getText().toString().length())
+                    if(txtTexto.getText().toString().equals(palabras[contador][palabra])) {
+                        contador++;
+                        txtTexto.setText("");
+                        if(contador==5){
+                            x=6000;
+                            return;
+                        }
+                        seleccionarPalabra();
+                    }
+            }
+        });
+
+
         this.btnBorrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!txtTexto.getText().toString().isEmpty())
-                        txtTexto.setText(txtTexto.getText().toString().substring(0,txtTexto.getText().toString().length()-1));
+            if(!txtTexto.getText().toString().isEmpty())
+                txtTexto.setText(txtTexto.getText().toString().substring(0,txtTexto.getText().toString().length()-1));
             }
         });
     }
 
+    private void seleccionarPalabra(){
+        this.palabra= (int) (Math.random()*5);
+        this.lblPalabra.setText(this.palabras[this.contador][this.palabra]);
+    }
     protected void onStart() {
         super.onStart();
         this.running=true;
